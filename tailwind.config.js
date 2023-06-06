f60dafd01474a54da02fb805dc727750995e35ae
Giacomo Moscardini <gmoscardini@webgriffe.com>
2023-06-06 20:07:18 +0200

    [REFACTOR] theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ['./templates/**/*.{twig,js,css}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./templates/**/*.twig', './node_modules/xtendui/src/*.mjs'],
+    plugins: [],
+    presets: [
+        require('tailwindcss/defaultConfig'), require('xtendui/tailwind.preset'),
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "color-1": 'rgb(239, 77, 72)',
+                "color-2": 'rgb(217, 7, 0)',
+                "color-3": 'rgb(43, 22, 27)',
+                "color-4": 'rgb(69, 62, 62)',
+                "color-5": 'rgb(247, 243, 245)'
+            },
+            fontFamily: {
+                sans: ['Cairo', ...defaultTheme.fontFamily.sans],
+                serif: ['Bungee', ...defaultTheme.fontFamily.serif]
+            }
+        },
+        xtendui: {
+            global: {
+                component: theme => ({
+                    '::selection': {
+                        backgroundColor: theme('colors.color-1'),
+                        color: theme('colors.color-3')
+                    },
+                }),
+            },
+        },
+    }
 }
